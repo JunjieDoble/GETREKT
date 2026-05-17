f4f4d4eb92cde525bbf5d0bcc71f76146b4181ed
HiPomelo <jzhou@edu.tecnocampus.cat>
2026-05-17 19:28:10 +0200

    Final level adjusting

---
+++ Assets/_Scripts/Final.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Final : MonoBehaviour
+{
+    public TextMeshProUGUI checkpointText;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            checkpointText.SetText("Congratulations! You've reached the end of the level!");
+            StartCoroutine(HideTextCoroutine());
+        }
+    }
+
+    IEnumerator HideTextCoroutine()
+    {
+        yield return new WaitForSeconds(2f);
+        checkpointText.SetText("");
+    }
+}
